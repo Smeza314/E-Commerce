f1478482994ae961696404ad86be7c5ad468d2f2
Smeza314 <sebastianmeza314@gmail.com>
2021-03-24 10:53:20 -0700

    finished product routes

--- routes/product-routes.js
+++ routes/product-routes.js
@@ -7,12 +7,23 @@ const { Product, Category, Tag, ProductTag } = require('../../models')
 router.get('/products', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    include: [Category, Tag]
+  })
+    .then(products => res.json(products))
+    .catch(err => console.log(err))
 })
 
 // get one product
 router.get('/products/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findone({
+    where: {id: req.params.body},
+    include: [Category, Tag]
+  })
+    .then(products => res.json(products))
+    .catch(err => console.log(err))
 })
 
 // create new product
@@ -91,6 +102,11 @@ router.put('/products/:id', (req, res) => {
 
 router.delete('/products/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {id: req.params.body}
+  })
+    .then(() => res.sendStatus(200))
+    .catch(err => console.log(err))
 })
 
 module.exports = router
