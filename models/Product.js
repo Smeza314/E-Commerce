ba7a27bca8b0696a59b2cd156d8c6060d0f0fb1b
Smeza314 <sebastianmeza314@gmail.com>
2021-04-01 12:05:49 -0700

    a

--- models/Product.js
+++ models/Product.js
@@ -32,7 +32,7 @@ Product.init(
       defaultValue: 10,
       numeric: true
     },
-    catergory_id: {
+    catergoty_id: {
       type: DataTypes.INTEGER,
       references: {
         model: Category,
